5f9c10ad8242fad96aa0aa93b2e49164d678535e
Minh Vo <minhvo4398@gmail.com>
2024-07-09 20:45:40 +0700

    working with multiple element

--- test/specs/android-fileElements.spec.js
+++ test/specs/android-fileElements.spec.js
@@ -39,4 +39,32 @@ describe("Android Element Tests", () => {
     // find by text contains
     await $('android=new UiSelector().textContains("Alert Dialogs")').click();
   });
+
+  it.only("Find multiple elements", async () => {
+    const expectedList = [
+      "API Demos",
+      "Access'ibility",
+      "Accessibility",
+      "Animation",
+      "App",
+      "Content",
+      "Graphics",
+      "Media",
+      "NFC",
+      "OS",
+      "Preference",
+      "Text",
+      "Views",
+    ];
+    const actualList = [];
+    // find multiple elements
+    const textList = await $$("android.widget.TextView");
+
+    // loop through them
+    for (const element of textList) {
+      actualList.push(await element.getText());
+    }
+    // assert the list
+    await expect(actualList).toHaveValue(expectedList);
+  });
 });
