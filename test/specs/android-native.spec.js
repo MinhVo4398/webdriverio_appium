826c0cf55761e47fbcfe086f5eb6a902de12f32b
Minh Vo <minhvo4398@gmail.com>
2024-07-10 09:01:57 +0700

    working with alert text

--- test/specs/android-native.spec.js
+++ test/specs/android-native.spec.js
@@ -1,12 +1,44 @@
-describe('Android Native Feature Tests', ()=>{
-    it('Access ac Activity directly', async()=>{
-        // access acitivity
-        await driver.startActivity("io.appium.android.apis","io.appium.android.apis.app.AlertDialogSamples");
+describe("Android Native Feature Tests", () => {
+  it("Access ac Activity directly", async () => {
+    // access acitivity
+    await driver.startActivity(
+      "io.appium.android.apis",
+      "io.appium.android.apis.app.AlertDialogSamples"
+    );
 
-        // pause 3s
-        await driver.pause(3000);
+    // pause 3s
+    await driver.pause(3000);
 
-        //assertion
-        await expect($('//android.widget.TextView[@text="App/Alert Dialogs"]')).toExist();
-    })
-})
+    //assertion
+    await expect(
+      $('//android.widget.TextView[@text="App/Alert Dialogs"]')
+    ).toExist();
+  });
+
+  it.only("Working with Dialog Boxes", async () => {
+    // access acitivity
+    await driver.startActivity(
+      "io.appium.android.apis",
+      "io.appium.android.apis.app.AlertDialogSamples"
+    );
+
+    // click on first dialog
+    await $('//*[@resource-id="io.appium.android.apis:id/two_buttons"]').click();
+
+    // Accept Alert
+   // await driver.acceptAlert();
+
+    // Dismiss Alert
+    // await driver.dismissAlert();
+
+    // Get Alert Text
+    console.log('ALERT TEXT----->', await driver.getAlertText());
+
+    // Or can click on OK button
+    await $('//android.widget.Button[@resource-id="android:id/button1"]').click();
+
+
+    // Assertion - alert box is no longer visible
+    await expect($('//*[@resource-id="android:id/alertTitle"]')).not.toExist();
+  });
+});
