4f1391eae71c22a91ef89e0cdf77e56d3db51835
Minh Vo <minhvo4398@gmail.com>
2024-07-10 18:45:18 +0700

    add delete note

--- test/specs/android/delete-note.spec.js
+++ test/specs/android/delete-note.spec.js
@@ -1,60 +1,65 @@
-/*
-describe('Add Notes', ()=>{
-    it('Skip tutorial', async ()=>{
-        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip"]').click();
-        await expect($('//*[@text="Add note"]')).toBeDisplayed();
-    })
+describe("Add Notes", () => {
+  it("Skip tutorial", async () => {
+    await $(
+      '//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip"]'
+    ).click();
+    await expect($('//*[@text="Add note"]')).toBeDisplayed();
+  });
 
-    it('add a note, save changes & verify notes', async()=>{
-        await $('//*[@text="Add note"]').click();
-        await $('//*[@text="Text"]').click();
-        await expect($('//*[@text="Editing"]')).toBeDisplayed();
+  it("add a note, save changes & verify notes", async () => {
+    await $('//*[@text="Add note"]').click();
+    await $('//*[@text="Text"]').click();
+    await expect($('//*[@text="Editing"]')).toBeDisplayed();
 
-        // Add note title
-        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_title"]')
-        .addValue("Fav Anime List");
+    // Add note title
+    await $(
+      '//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_title"]'
+    ).addValue("Fav Anime List");
 
-        // Add note body
-       await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_note"]')
-        .addValue("Naruto\nOnePiece\nAOT");
+    // Add note body
+    await $(
+      '//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_note"]'
+    ).addValue("Naruto\nOnePiece\nAOT");
 
-        //save the changes (click back twice)
+    //save the changes (click back twice)
 
-        await driver.back();
-        await driver.back();
+    await driver.back();
+    await driver.back();
+  });
 
-        // assertion
-        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_btn"]')).toBeDisplayed();
-        await expect($('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/view_note"]')).toHaveText("Naruto\nOnePiece\nAOT");
+  it("Delete a note & check the note in trash can", async () => {
+    await driver.back();
+    const note = await $(
+      '//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'
+    ).getText();
 
-    })
+    // Click on the note
+    await $(
+      '//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'
+    ).click();
 
-    it('Delete a note & check the note in trash can', async()=> {
-        const note = await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]').getText();
+    // Click on more icon
+    await $("~More").click();
 
-        // Click on the note
-         await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]').click();
-        
-         // Click on more icon
-         await $(~'More').click();
+    // Click on delete item
+    await $('//*[@text="Delete"]').click();
 
-         // Click on delete item
-         await $('//[@text="Delete"]').click();
+    //accept alert
+    await driver.acceptAlert();
 
-         //accept alert
-         driver.acceptAlert();
+    // click nav icon
+    await $(
+      '//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/icon_nav"]'
+    ).click();
 
-        // click nav icon
-        await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/icon_nav"]').click();
-        
-        // Click on trash can item
-        await $('//*[@text="Trash Can"]').click();
+    // Click on trash can item
+    await $('//*[@text="Trash Can"]').click();
 
-        // Assertiion
-        const trashCanItem =  await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]').getText();
+    // Assertion
+    const trashCanItem = await $(
+      '//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/title"]'
+    );
 
-        await expect(trashCanItem).toHaveText(note);
-    })
-})
-
-*/
+    await expect(trashCanItem).toHaveText(note);
+  });
+});
