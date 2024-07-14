3215d3df2b0e06b772f8fb195faf4504181f479f
Minh Vo <minhvo4398@gmail.com>
2024-07-14 15:40:29 +0700

    class chain

--- test/specs/ios-findElement.spec.js
+++ test/specs/ios-findElement.spec.js
@@ -1,26 +1,40 @@
-describe('iOS Find Element', ()=>{
-    it('find element by accessibility id', async ()=>{
-        await $('~Alert Views').click();
-        await $('~Simple').click();
-        await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
-    });
+describe("iOS Find Element", () => {
+  it("find element by accessibility id", async () => {
+    await $("~Alert Views").click();
+    await $("~Simple").click();
+    await expect(await driver.getAlertText()).toContain(
+      "A Short Title Is Best"
+    );
+  });
 
-    it('find by tag name', async ()=>{
-        // single element
-        console.log(await $('XCUIElementTypeStaticText').getText());
+  it("find by tag name", async () => {
+    // single element
+    console.log(await $("XCUIElementTypeStaticText").getText());
 
-        // multiple elements
-        const textEls = await $$('XCUIElementTypeStaticText');
-        
-        for (const ele of textEls) {
-            console.log(await ele.getText());
-        }
-    })
+    // multiple elements
+    const textEls = await $$("XCUIElementTypeStaticText");
 
-    it.only('find element by xpath ', async ()=>{
-        // xpath - (//tagname[@attribute=value])
-        await $('//*[@name="Alert Views"]').click();
-        await $('//*[@label="Simple"]').click();
-        await expect(await driver.getAlertText()).toContain("A Short Title Is Best");
-    } )
+    for (const ele of textEls) {
+      console.log(await ele.getText());
+    }
+  });
+
+  it("find element by xpath ", async () => {
+    // xpath - (//tagname[@attribute=value])
+    await $('//*[@name="Alert Views"]').click();
+    await $('//*[@label="Simple"]').click();
+    await expect(await driver.getAlertText()).toContain(
+      "A Short Title Is Best"
+    );
+  });
+
+  it.only("find element by class chain", async () => {
+    //const alertText = '**/XCUIElementTypeStaticText[`name == "Alert Views"`]';
+    const alertText = '**/XCUIElementTypeStaticText[`label CONTAINS "Alert"`]';
+    await $(`-ios class chain:${alertText}`).click();
+    await $('//*[@label="Simple"]').click();
+    await expect(await driver.getAlertText()).toContain(
+      "A Short Title Is Best"
+    );
+  });
 });
