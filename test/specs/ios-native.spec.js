d9ba3d83d5d65cc17e2cda61c9f789dc2041fa76
Minh Vo <minhvo4398@gmail.com>
2024-07-14 17:56:34 +0700

    picker view

--- test/specs/ios-native.spec.js
+++ test/specs/ios-native.spec.js
@@ -15,7 +15,7 @@ describe("iOS Native Features", () => {
     await expect($("~OK")).not.toExist();
   });
 
-  it.only("Working with Scrollable elements", async () => {
+  it("Working with Scrollable elements", async () => {
     //easiest
     //await driver.execute('mobile: scroll', {direction:"down"});
     //await driver.execute('mobile: scroll', {direction:"up"});
@@ -33,6 +33,23 @@ describe("iOS Native Features", () => {
         element: bluePicker.elementId,
         direction: "up",
       });
+    await driver.pause(2000);
+  });
+
+  it.only("Working with Picker view", async () => {
+    // complex
+    await $("~Picker View").click();
+
+    const redPicker = await $("~Red color component value");
+    const greenPicker = await $("~Green color component value");
+    const bluePicker = await $("~Blue color component value");
+
+    // Set purple color (125, , 0, 125)
+    await redPicker.addValue('125');
+    await greenPicker.addValue('0')
+    await bluePicker.addValue('125')
+    
+
     await driver.pause(2000);
   });
 });
